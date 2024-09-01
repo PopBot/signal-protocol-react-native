2ef0f430798b5d278d3bca427a8542139fd406c5
Jarod Wong <popbot@users.noreply.github.com>
2024-09-01 12:48:16 -0700

    Adding editor configuration files.

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	bracketSpacing: true,
+	bracketSameLine: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'always',
+	tabWidth: 4,
+	useTabs: true,
+	singleAttributePerLine: true,
+	jsxSingleQuote: true,
+	semi: true,
+	quoteProps: 'consistent',
+	endOfLine: 'lf',
+	embeddedLanguageFormatting: 'auto',
+	proseWrap: 'preserve',
+	printWidth: 80,
+	// Apply this to json files as well
+	overrides: [
+		{
+			files: '*.json',
+			options: {
+				tabWidth: 4,
+			},
+		},
+	],
+};
